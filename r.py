2539d422a30aa256a7d6e75c78fcabfd5eb235a0
Manoj Myneni <68726356+man0j-012@users.noreply.github.com>
2025-05-05 16:15:14 -0500

    Delete r.py

--- r.py
+++
@@ -1,7 +0,0 @@
-print("Percentage of Invalid query results: 2 and % 6.70")
-print("Percentage of Invalid query results: 3 and % 5.00")
-print("Percentage of Invalid query results: 4 and % 4.40")
-print("Percentage of Invalid query results: 2 and % 5.00")
-print("Percentage of Invalid query results: 3 and % 6.00")
-print("Percentage of Invalid query results: 5 and % 10.00")
-print("Percentage of Invalid query results: 8 and % 13.33")
